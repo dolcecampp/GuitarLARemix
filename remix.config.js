428c71e5851e5f19b737f010fabd682abdd2a94e
agenciamarkeen <agenciamarkeen@gmail.com>
2023-07-26 08:46:21 +0200

    probando para deploy

--- remix.config.js
+++ remix.config.js
@@ -1,11 +1,14 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  ignoredRouteFiles: ["**/.*"],
+  serverBuildTarget: 'vercel',
+
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
-  serverModuleFormat: "cjs",
+  server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
+  serverModuleFormat: 'cjs',
+  ignoredRouteFiles: ['**/.*'],
   future: {
     v2_dev: true,
     v2_errorBoundary: true,
